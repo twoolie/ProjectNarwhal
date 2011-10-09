b23d84c37deb6d869711005757b0029fb9e21420
twoolie <woolford.thomas@gmail.com>
2011-10-09 17:59:31 +1030

    added `bootstrap` and `deploy` commands for easy upload to non ep.io server (only apache supported so far)

--- fabfile.py
+++ fabfile.py
@@ -6,13 +6,14 @@ from argparse import ArgumentError
 
 env.hosts = ['your_username@yourhost:yourport']
 env.web_path = '/var/www/django'
-env.log_root ='/var/log/apache/'
+env.log_root ='/var/log/apache'
+env.project = 'example_project'
 
 @task
 def dev_serve():
     with prefix('Scripts\\activate.bat' if platform.system()=="Windows" else 'source bin/activate'):
-        local(join('compass watch example_project','static &'))
-        local(join('python example_project','manage.py runserver'))
+        local(join('compass watch %(project)s','static &') % env)
+        local(join('python %(project)s','manage.py runserver') % env)
 
 @task
 def bootstap_dev():
@@ -22,45 +23,43 @@ def bootstap_dev():
         local('virtualenv --distribute .')
         with prefix('Scripts\\activate.bat' if platform.system()=="Windows" else 'source bin/activate'):
             local('pip install -r requirements.txt')
-            local(join('python example_project','manage.py syncdb'))
-            local(join('python example_project','manage.py migrate'))
+            local(join('python %(project)s','manage.py syncdb') % env)
+            local(join('python %(project)s','manage.py migrate') % env)
 
 @task
-def bootstrap(hostname, path=env.web_path, apache_conf_path=None, distro=None, log_root=env.log_root):
+def bootstrap(hostname, path=env.web_path, **kwargs):
     """ Creates a virtualhost instance on the box you specify 
-        `fab -H server1,server2 bootstrap:narwhal.example.com` """
-    host=env.host_string
-    if not apache_conf_path: apache_conf_path=find_apache_path(distro)
+        `fab -H server1,server2 bootstrap:narwhal.example.com[,deploy_options...]` """
     
-    run('mkdir -p %(path)s/%(hostname)s/')
-    with cd('%(path)s/%(hostname)s/'):
+    run('mkdir -p %(path)s/%(hostname)s/' % locals())
+    with cd('%(path)s/%(hostname)s/' % locals()):
         run('git init .')
     
-    deploy(hostname)
+    locals().update(kwargs)
+    deploy(**locals()) # deploy script takes care of the rest
 
 @task
-def deploy(hostname, ref='master', path=env.web_path, apache_conf_path=None, distro=None, log_root=env.log_root):
-    """ ` fab -H server1,server2 deploy:narwhal.example.com` """
-    host=env.host_string
+def deploy(hostname, ref='master', path=env.web_path, apache_conf_path=None, distro=None, \
+           log_root=env.log_root, thread_count=2, process_count=4):
+    """ `fab -H server1,server2 deploy:narwhal.example.com` """
     if not apache_conf_path: apache_conf_path=find_apache_path(distro)
     
-    local('git push -f ssh://%(host)s/%(path)s/%(hostname)s/ %(ref)s' % locals())
-    with cd(_join(path, hostname)):
-        run('git checkout -f %ref' % locals())
+    local('git push -f ssh://%(host_string)s/%(path)s/%(hostname)s/ %(ref)s' % locals())
+    with cd('%(path)s/%(hostname)s' % locals()):
+        run('git checkout -f %(ref)s' % locals())
         run('pip install -r requirements.txt')
-        files.upload_template("example_project/apache.conf", apache_conf_path+hostname,
-                              context=locals(), mode=0755, use_sudo=True)
-        with cd('example_project'):
+        with cd(env.project):
+            files.upload_template('apache.conf', apache_conf_path+hostname,
+                                  context=locals(), mode=0755, use_sudo=True)
             run('./manage.py collectstatic --noinput')
             run('./manage.py syncdb')
             run('./manage.py migrate')
-            run('touch serve.py') # restart the wsgi process
+            run('touch serve.wsgi') # restart the wsgi process
 
 @task
 def deploy_epio(appname):
     """ fab deploy_epio:appname """
-    with lcd('example_project'), prefix('Scripts\\activate.bat' if platform.system()=="Windows" else 'source bin/activate'):
-        local('python manage.py rebuild_solr_index')
+    with lcd(env.project), prefix('Scripts\\activate.bat' if platform.system()=="Windows" else 'source bin/activate'):
         local('python manage.py collectstatic --noinput')
         local( ('mklink /D %(link)s %s(target)' if platform.system()=="Windows" else 'ln -s %(target)s %(link)s') \
                     % { 'link':'narwhal', 'target':'../narwhal' })
@@ -68,7 +67,8 @@ def deploy_epio(appname):
         local('epio upload -a %(appname)s'%locals())
         local('epio django syncdb -a %(appname)s'%locals())
         local('epio django migrate -a %(appname)s'%locals())
-        
+        local('epio django rebuild_index')
+
 
 #-------- Utils ----------
 
